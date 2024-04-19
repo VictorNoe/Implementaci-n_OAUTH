739031f3f28ff54672c019472ee52f8498a3c350
VictorNoe <20213tn059@utez.edu.mx>
2024-04-18 22:19:15 -0600

    Add oauth

---
+++ src/main.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import router from './router';
+import { domain, clientId } from '../auth_config.json';
+import { Auth0Plugin } from './auth';
+
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  onRedirectCallback: appState => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    );
+  }
+});
+
+Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+new Vue({
+  router,
+  render: (h) => h(App)
+}).$mount('#app')
