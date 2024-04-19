26ce008010a90127e9171b365d2e3aca8f71dad1
VictorNoe <20213tn059@utez.edu.mx>
2024-04-18 22:47:37 -0600

    Change user info

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="home">
-        <h1>Login con oauth</h1>
-        <!-- Revisa la librería se encuentra cargando antes de mostrar los botones de acceso -->
-        <div v-if="!$auth.loading">
-            <!-- muestra el botón de login si no está autentificado -->
-            <button v-if="!$auth.isAuthenticated" @click="login">Log in</button>
-            <!-- muestra el botón de Log out si está autentificado -->
-            <button v-if="$auth.isAuthenticated" @click="logout">Log out</button>
-        </div>
-
-    </div>
+    <b-container class="home">
+        <b-row>
+            <b-col>
+                <h1>Login con oauth</h1>
+                <div v-if="!$auth.loading">
+                    <button class="btn btn-success" v-if="!$auth.isAuthenticated" @click="login">Log in</button>
+                    <button class="btn btn-danger" v-if="$auth.isAuthenticated" @click="logout">Log out</button>
+                </div>
+            </b-col>
+        </b-row>
+    </b-container>
 </template>
 
 <script>
--- src/view/Profile.vue
+++ src/view/Profile.vue
@@ -1,13 +1,24 @@
 <template>
-    <div>
-      <div>
-        <img :src="$auth.user.picture" />
+  <b-container>
+    <b-row class="text-center">
+      <b-col cols="12">
+        <img :src="$auth.user.picture"/>
+      </b-col>
+      <b-col cols="12">
         <h2>{{ $auth.user.name }}</h2>
         <p>{{ $auth.user.email }}</p>
-      </div>
-  
-      <div>
-        <pre>{{ JSON.stringify($auth.user, null, 2) }}</pre>
-      </div>
-    </div>
-  </template>
+      </b-col>
+      <b-col cols="12">
+        <div>
+          <p>{{ $auth.user.given_name }}</p>
+          <p>{{ $auth.user.family_name }}</p>
+          <p>{{ $auth.user.nickname }}</p>
+          <p>{{ $auth.user.locale }}</p>
+          <p>{{ $auth.user.updated_at }}</p>
+          <p>{{ $auth.user.email_verified }}</p>
+          <p>{{ $auth.user.sub }}</p>
+        </div>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
